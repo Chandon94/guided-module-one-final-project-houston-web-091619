6431f094bd47edc2c6ddaa98dd6b18331af576a4
fernandocgomez <fernandocgomez@live.com>
2019-10-04 13:01:20 -0500

    final cheanges

--- run.rb
+++ run.rb
@@ -8,15 +8,18 @@ prompt = TTY::Prompt.new
 a = Artii::Base.new :font => 'slant'
 
 
-# 100.times do |i|
-#     progressbar.increment
-  
-#     if i == 72 
-#       progressbar.format = "%t: |\e[31m%B\e[0m|"
-#     end
-  
-#     sleep 0.1
-# end
+progressbar = ProgressBar.create(format: "\e[0m%t: |%B|\e[0m")
+
+100.times do |i|
+  progressbar.increment
+
+  if i == 25 
+    progressbar.format = "%t: |\e[31m%B\e[0m|"
+  end
+
+
+  sleep 0.1
+end
 
 puts a.asciify('Welcome')
 
