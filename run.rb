d90923f1a34f3190b52a528d047887013d9467d9
fernandocgomez <fernandocgomez@live.com>
2019-10-04 11:27:15 -0500

    project complted

--- run.rb
+++ run.rb
@@ -61,15 +61,25 @@ while user == nil
                 bank: bank
             })
 
-            user_response = "Login"
+            puts a.asciify("Account Succesfully Created")
+
         end        
 
         if user_response == "Login"
+
             p "Please Login on your account"
-            current_ninja = Ninja.where({
-            name: prompt.ask("Enter your name:"),
-            password: prompt.mask("Enter password:")
-        })
+            current_ninja = Ninja.find_by(name: prompt.ask("Enter your name:"))
+            password_ninja = prompt.mask("Enter password:")
+
+            while  password_ninja != current_ninja.password
+                puts "Error Wrong Password! Try Again"
+                # password_ninja != current_ninja.password
+                password_requester = password_ninja = prompt.mask("Enter password:")
+            end
+
+        
+
+
         end
 
         while user_response = prompt.select("What would you like to do?",[
@@ -80,13 +90,60 @@ while user == nil
             if user_response == "Exit"
                 break
             end
-            #options for the ninja goes here
+            #options for the ninja goes 
+            
+            if user_response == "See My Missions And Update Status"
+                # binding.pry
+                if current_ninja.missions == []
 
-        end
+                    
+                    puts a.asciify("You have no missions")
+                
+                else
+                    mission_status_array = current_ninja.missions.map do |mission|
+                        mission.title
+                    end 
+                    
+                 
+                    new_status = prompt.select("Choose mission to be updated", mission_status_array)
+    
+                    if new_status == new_status
+                        mission_option_status = ["Ongoing", "Pending", "Fail", "Completed"]
+    
+                        new_status_for_mission = prompt.select("Choose the new status for the mission", mission_option_status)
+    
+                        missioin_new_status_change = Mission.find_by(title: new_status)
+                        missioin_new_status_change.update(status: new_status_for_mission)
+    
+                        puts a.asciify("New Mission Status Is #{new_status_for_mission}")
+    
+                    end
+                end
+              
+                 
+                
+                
+
+                 
+
+            end #end of See My Missions And Update Status
+
+            if user_response == "Check My Available Balance"
+
+                current_ninja_id = Ninja.all.find do |ninja|
+                    ninja.id == current_ninja.id
+                end
+
+                puts a.asciify("Current Balnce is $#{current_ninja_id.bank}")
+
+             end #end of Check My Available Balance
+
+        end #end of the loop
 
         
     end
 
+#  **************************************REQUESTERS************************************************************************
 
 
     if user_response == "Requester"
@@ -109,22 +166,29 @@ while user == nil
                 funds: funds
             })
 
-            user_response = "Login"
+            puts a.asciify("Account Succesfully Created")
+
         end        
 
         if user_response == "Login"
             p "Please Login on your account"
-            current_requester = Requester.where({
-            name: prompt.ask("Enter your name:"),
-            password: prompt.mask("Enter password:")
-        })
+            current_requester = Requester.find_by(name: prompt.ask("Enter your name:"))
+            password_requester = prompt.mask("Enter password:")
+
+            while  password_requester != current_requester.password
+                puts "Error Wrong Password! Try Again"
+                # password_ninja != current_ninja.password
+                password_requester = password_requester = prompt.mask("Enter password:")
+            end
+            
         end
+
 
         #options for the requester goes here
 
         while user_response = prompt.select("What would you like to do?",[
             "Create A Mission.",
-            "See List Of Available Ninjas.",
+            "See List Of All Available Ninjas.",
             "Check Status Of My Current Missions.",
             "Exit" ])
 
@@ -132,7 +196,7 @@ while user == nil
                 break
             end
 
-            if user_response == "Create A Mission!"
+            if user_response == "Create A Mission."
 
                 title = prompt.ask("What's the title of the mission?")
 
@@ -170,14 +234,8 @@ while user == nil
                     
                   
                 end
-        
-                req = Requester.find_by(id: current_requester.id)
-                new_value = req.funds - cost
-                req.update(funds: new_value) 
                 
                 status = "Pending"
-        
-                # prompt.say('Choose a ninja to complete this misson')
                 
                 ninjas_hash = {}
                 alive = Ninja.all.select do |alive|
@@ -188,6 +246,10 @@ while user == nil
                 end
         
                 ninja_id = prompt.select("Choose a Ninja!", ninjas_hash)
+
+                req = Requester.find_by(id: current_requester.id)
+                new_value = req.funds - cost
+                req.update(funds: new_value) 
         
                 current_requester = Mission.create({
                     title: title,
@@ -203,11 +265,73 @@ while user == nil
                 nin = Ninja.find_by(id: ninja_id)
                 pay_nin = nin.bank + cost
                 nin.update(bank: pay_nin)
-        
-                print "Mission succesfuly crated"
+
+                puts a.asciify("Mission succesfuly created")
         
         
             end #create a mission end
+
+            if user_response == "See List Of All Available Ninjas."
+                
+                ninjas_hash = {}
+
+                alive = Ninja.all.select do |alive|
+                    alive.status == "Alive"
+                end
+
+                alive.each do |ninja|
+                    ninjas_hash[ninja.name] = ninja.id
+                end
+
+                prompt.select("Choose a Ninja!", ninjas_hash)
+
+            end # See list of all aviable nijas end
+           
+            if user_response == "Check Status Of My Current Missions."
+                
+                
+                mission_hash_requester = {}
+                
+                mission_array_requester = current_requester.missions.map do |mission|
+                    mission.title
+                end 
+
+                selected_mission = prompt.select("Choose the mission to see the current status", mission_array_requester)
+
+                requester_mission_instance = Mission.find_by(title: selected_mission)
+
+                puts a.asciify("#{requester_mission_instance.status}")
+
+                print requester_mission_instance.status
+
+                # binding.pry
+
+                # base on the name of the mission find the status of the missiona and print it
+
+                
+                # *************************************************************
+                
+                # mission_status_array = current_requester.missions.map do |mission|
+                #     mission.title
+                # end 
+
+                # new_status = prompt.select("Choose mission to be updated", mission_status_array)
+
+                # if new_status == new_status
+                #     mission_option_status = ["Ongoing", "Pending", "Fail", "Completed"]
+
+                #     new_status_for_mission = prompt.select("Choose the new status for the mission", mission_option_status)
+
+                #     missioin_new_status_change = Mission.find_by(title: new_status)
+                #     missioin_new_status_change.update(status: new_status_for_mission)
+
+                    
+
+                #  end
+
+                
+
+            end # Check Status Of My Current Missions. end
 
         end
         
@@ -215,3 +339,4 @@ while user == nil
 
 
 end
+
